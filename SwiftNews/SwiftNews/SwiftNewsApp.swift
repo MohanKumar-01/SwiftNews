039a05edf232ce41bd99f6808d63399c96a1e99c
Mohan <mohankumarbandaru.01@gmail.com>
2025-07-15 21:29:59 +0530

    Initial Setup

---
+++ SwiftNews/SwiftNews/SwiftNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftNewsApp.swift
+//  SwiftNews
+//
+//  Created by Mohan Kumar on 15/07/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftNews/SwiftNewsTests/SwiftNewsTests.swift
@@ -0,0 +1,16 @@
+//
+//  SwiftNewsTests.swift
+//  SwiftNewsTests
+//
+//  Created by Mohan Kumar on 15/07/25.
+//
+
+import Testing
+
+struct SwiftNewsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
